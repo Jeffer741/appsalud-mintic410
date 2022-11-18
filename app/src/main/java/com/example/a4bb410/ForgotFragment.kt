9ee2c3df969da8d335680290b32091d8190aed38
Jeffer741 <jfcm.op4783@gmail.com>
2022-11-17 19:31:08 -0500

    nav
    creacion de navegacion del forgot al signupy de signup a login

--- app/src/main/java/com/example/a4bb410/ForgotFragment.kt
+++ app/src/main/java/com/example/a4bb410/ForgotFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.a4bb410.databinding.FragmentForgotBinding
 
 
@@ -25,6 +26,14 @@ class ForgotFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentForgotBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.fragmentForgotLabel2.setOnClickListener {
+            findNavController().navigate(R.id.action_forgotFragment_to_signUpFragment)
+
+        }
     }
 
 
