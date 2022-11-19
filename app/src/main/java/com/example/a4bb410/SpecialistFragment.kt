419bac506b794fc4ecb5c954ea9736e16585d668
Jeffer741 <jfcm.op4783@gmail.com>
2022-11-18 23:54:15 -0500

    creacion fragmentos privados
    home, location, Profile, Specialist

---
+++ app/src/main/java/com/example/a4bb410/SpecialistFragment.kt
@@ -0,0 +1,33 @@
+package com.example.a4bb410
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a4bb410.databinding.FragmentSignUpBinding
+import com.example.a4bb410.databinding.FragmentSpecialistBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SpecialistFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SpecialistFragment : Fragment() {
+
+    private var _binding: FragmentSpecialistBinding? = null
+    private val binding: FragmentSpecialistBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSpecialistBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
+
