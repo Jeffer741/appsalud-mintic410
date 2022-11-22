7e8e484c9859196344d12a37815254e622b3a199
Jeffer741 <jfcm.op4783@gmail.com>
2022-11-22 11:01:11 -0500

    navegacion privada y creaccion container
    se habilito la navegación entre las vistas internas reciclando los componentes como titulo, subtitulo y descripción y se creo el container view para desplegar opciones

--- app/src/main/java/com/example/a4bb410/SpecialistFragment.kt
+++ app/src/main/java/com/example/a4bb410/SpecialistFragment.kt
@@ -1,10 +1,13 @@
 package com.example.a4bb410
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.navArgs
 import com.example.a4bb410.databinding.FragmentSignUpBinding
 import com.example.a4bb410.databinding.FragmentSpecialistBinding
 
@@ -18,6 +21,7 @@ class SpecialistFragment : Fragment() {
 
     private var _binding: FragmentSpecialistBinding? = null
     private val binding: FragmentSpecialistBinding get() = _binding!!
+    private val args: SpecialistFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +30,21 @@ class SpecialistFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSpecialistBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+       if (args.search) {
+           binding.specialistFragmentRecyclerSearchLayout.visibility = View.VISIBLE
+           binding.specialistFragmentRecyclerTitle.visibility = View.GONE
+           binding.specialistFragmentTitle.text = getString(R.string.specilist_title)
+           binding.specialistFragmentSubtitle.text = getString(R.string.specilist_subtitle)
+       } else {
+           binding.specialistFragmentRecyclerSearchLayout.visibility = View.GONE
+           binding.specialistFragmentRecyclerTitle.visibility = View.VISIBLE
+           binding.specialistFragmentTitle.text = args.name
+           binding.specialistFragmentSubtitle.text = args.description
+       }
     }
 
 
